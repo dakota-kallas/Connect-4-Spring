2c777acd890a8926ec3ee7dfbbc7de21184adfa9
Dakota Kallas <kallas6990@uwlax.edu>
2023-04-02 17:15:38 -0500

    Started security

---
+++ server/src/main/java/com/dk/server/security/BadRequestException.java
@@ -0,0 +1,10 @@
+package com.dk.server.security;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST, reason="Bad Request")
+public class BadRequestException extends Exception {
+}
+
+
